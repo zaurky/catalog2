1e7e158c35206d0e76ad973390d4e6e8dd024f81
zaurky <zaurky@zeb.re>
2012-03-30 16:51:30 +0200

    add a basic view for camera catalog list

--- camera/views.py
+++ camera/views.py
@@ -1,1 +1,16 @@
+from django.template import Context, loader
+from catalog2.camera.models import Catalog
+from django.http import HttpResponse
+
 # Create your views here.
+
+def list(request):
+    catalog_list = Catalog.objects.all()
+    t = loader.get_template('camera/list.html')
+    c = Context({
+        'catalog_list': catalog_list,
+    })
+    return HttpResponse(t.render(c))
+
+def info(request, catalog_id):
+    return
