152a4548c5f7e72a46e02efa20e876ad0c76bec0
zaurky <zaurky@zeb.re>
2012-03-30 16:52:03 +0200

    ref templates

--- settings.py
+++ settings.py
@@ -103,6 +103,8 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'catalog2.urls'
 
 TEMPLATE_DIRS = (
+    "/home/olivier/Perso/catalog2/catalog2/templates",
+    "/home/olivier/Perso/catalog2/catalog2/camera/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
