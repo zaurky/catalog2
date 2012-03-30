4c5477281a4c3ff2af46240da05dd283295a2ed1
zaurky <zaurky@zeb.re>
2012-03-30 16:52:30 +0200

    add url management for camera

--- urls.py
+++ urls.py
@@ -14,4 +14,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+
+    # cameras
+    url(r'^camera/', 'camera.views.list'),
+    url(r'^camera/(?P<catalog_id>\d+)/$', 'camera.views.info'),
 )
