086143b892931a8aa8c639fd2bd76dc8fccd7e66
zaurky <zaurky@zeb.re>
2012-04-14 11:51:13 +0200

    clean url file

--- urls.py
+++ urls.py
@@ -5,14 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'catalog2.views.home', name='home'),
-    # url(r'^catalog2/', include('catalog2.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    # admin
     url(r'^admin/', include(admin.site.urls)),
 
     # login
