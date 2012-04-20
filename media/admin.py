ca81687c570101a7d1be8a774cc575e44bc0c9f5
zaurky <zaurky@zeb.re>
2012-04-20 10:59:53 +0200

    add admin on media

---
+++ media/admin.py
@@ -0,0 +1,7 @@
+from catalog2.media.models import Tag, Media, Camera, Exemple
+from django.contrib import admin
+
+admin.site.register(Tag)
+admin.site.register(Media)
+admin.site.register(Camera)
+admin.site.register(Exemple)
