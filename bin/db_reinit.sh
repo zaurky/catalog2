3178ca9ad8601d18f419e1062f807c961ec1c11a
zaurky <zaurky@zeb.re>
2012-04-30 11:35:51 +0200

    sometime we get fixture from other files, in that case, just remove data

--- bin/db_reinit.sh
+++ bin/db_reinit.sh
@@ -4,14 +4,20 @@
 
 for TABLE in $TABLES; do
     TABLE=`echo $TABLE | sed -e 's/#/_/g'`
+    echo "DROP TABLE $TABLE"
     sqlite3 db/catalog2.sqlite3 "DROP TABLE $TABLE;"
 done
 
 python manage.py syncdb
 
-for TABLE in $TABLES; do
-    TABLE=`echo $TABLE | sed -e 's/.*#//'`
+for TABLE1 in $TABLES; do
+    TABLE=`echo $TABLE1 | sed -e 's/.*#//'`
+    SQLTABLE=`echo $TABLE1 | sed -e 's/#/_/g'`
     echo $TABLE
+    if [ `sqlite3 db/catalog2.sqlite3 "SELECT COUNT(*) FROM $SQLTABLE;"` -ne 0 ]; then
+	echo 'delete from'
+        sqlite3 db/catalog2.sqlite3 "DELETE FROM $SQLTABLE;"
+    fi
     python manage.py loaddata $TABLE
 done
 
