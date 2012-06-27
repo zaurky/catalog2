af9deb16c3761f8d4be71f0d65c6d84bb90922a5
zaurky <zaurky@zeb.re>
2012-06-27 17:06:21 +0200

    complete stats

--- other/views.py
+++ other/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
-from catalog2.film.models import Catalog, Life, InCamera, Sensitivity
-from catalog2.camera.models import Catalog as CameraCatalog
+from catalog2.film.models import Catalog, Life, InCamera, Sensitivity, Ref
+from catalog2.camera.models import (Catalog as CameraCatalog,
+    Model as CameraModel)
+
 from catalog2.contact.models import Developer
 from catalog2.develop.models import Product
 
@@ -9,6 +11,9 @@ from django.shortcuts import render_to_response, get_object_or_404
 def stats(request):
     return render_to_response('stats/info.html', {
         'camera_num': CameraCatalog.objects.count(),
-        'film_num': InCamera.objects.count(),
+        'camera_model': CameraModel.objects.count(),
+        'film_num': Life.objects.count(),
+        'film_ref': Ref.objects.count(),
+        'film_done': InCamera.objects.count(),
         'total_price': CameraCatalog.total_sum(),
     })
