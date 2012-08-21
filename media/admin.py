ac7e74c6858a345f5b98865382b117a3b1d5fa64
zaurky <zaurky@zeb.re>
2012-08-21 10:27:04 +0200

    add FilmSheet in media admin page

--- media/admin.py
+++ media/admin.py
@@ -1,7 +1,8 @@
-from catalog2.media.models import Tag, Media, Camera, Exemple
+from catalog2.media.models import Tag, Media, Camera, Exemple, FilmSheet
 from django.contrib import admin
 
 admin.site.register(Tag)
 admin.site.register(Media)
+admin.site.register(FilmSheet)
 admin.site.register(Camera)
 admin.site.register(Exemple)
