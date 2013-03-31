f560eb37faf164cc146c819e06e2bd4c7df2e93d
zaurky <zaurky@zeb.re>
2013-03-31 20:13:09 +0200

    lens should be in admin too

---
+++ lens/admin.py
@@ -0,0 +1,7 @@
+from catalog2.lens.models import Brand, Model, Catalog, LensMount
+from django.contrib import admin
+
+admin.site.register(Brand)
+admin.site.register(Model)
+admin.site.register(Catalog)
+admin.site.register(LensMount)
