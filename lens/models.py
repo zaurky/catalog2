f13e57551f13fb4f3f76af10a951775d74b9e23a
zaurky <zaurky@zeb.re>
2013-03-31 19:23:22 +0200

    add lenses models

--- lens/models.py
+++ lens/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.db.models import Sum
+from datetime import datetime
+
+from catalog2.contact.models import Contact
 
 # Create your models here.
+class LensMount(models.Model):
+    name = models.CharField(max_length=255)
+
+    @property
+    def lenses(self):
+        return Catalog.objects.filter(mount=self)
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Model(models.Model):
+    lense_brand = models.ForeignKey(Brand)
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return "%s %s" % (self.lense_brand, self.name)
+
+
+class Catalog(models.Model):
+    lense_model = models.ForeignKey(Model)
+    who = models.ForeignKey(Contact, default=6, related_name='lens')
+
+    date = models.DateTimeField(default=datetime.now)
+    is_borrowed = models.BooleanField(default=False)
+    given = models.BooleanField(default=False)
+    gift = models.BooleanField(default=False)
+
+    WORKING_CHOICES = (
+        ('Y', 'yes'),
+        ('N', 'no'),
+        ('M', 'maybe'),
+        ('L', 'maybe not')
+    )
+    working = models.CharField(max_length=1, choices=WORKING_CHOICES)
+
+    price = models.IntegerField(default=0)
+    sn = models.CharField(max_length=255, null=True, blank=True)
+    comment = models.CharField(max_length=1024, null=True, blank=True)
+
+    mount = models.ForeignKey(LensMount)
+
+    def __unicode__(self):
+        label = unicode(self.lense_model)
+        label += " : %s" % self.sn if self.sn else ''
+        label += " (%s)" % self.comment if self.comment else ''
+        return label
