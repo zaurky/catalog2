968d5a760651cb2d4489c7391ee9baea3676b629
zaurky <zaurky@zeb.re>
2013-04-12 14:21:04 +0200

    add homemade flag on Developper

--- contact/models.py
+++ contact/models.py
@@ -14,6 +14,7 @@ class Developer(models.Model):
     name = models.CharField(max_length=255)
     town = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
+    homemade = models.BooleanField(default=False)
 
     def __unicode__(self):
         return "%s [%s - %s]" % (self.name, self.town, self.country)
